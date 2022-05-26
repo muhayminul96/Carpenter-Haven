cc7e66f9c54c85de29203ef9a24678868b0906c3
muhayminul <muhayminul96@gmail.com>
2022-05-26 22:25:20 +0600

    added succesfully

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -2,22 +2,51 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../Shared/Loading";
+import { useForm } from "react-hook-form";
+import { serverUrl } from "../../helper";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { toast } from "react-toastify";
 
 const Purchase = () => {
-    const { id } = useParams(); 
-    console.log(id)
+  const { id } = useParams();
+  const [user, loading, error] = useAuthState(auth);
+
+  const { register, handleSubmit } = useForm();
+  console.log(id);
   const { isLoading, data: product } = useQuery("product", () =>
-    fetch(`http://localhost:5000/product/${id}`).then((res) => res.json())
+    fetch(`${serverUrl}product/${id}`).then((res) => res.json())
   );
-  if(isLoading){
-      return <Loading></Loading>
+  if (isLoading) {
+    return <Loading></Loading>;
   }
+
+  const onSubmit = (data) => {
+      const minimumOrderQuantity = parseInt(product.minimumOrderQuantity)
+      const orderQuantity = parseInt(data.orderQuantity)
+      const quantity = parseInt(product.quantity)
+      console.log(minimumOrderQuantity,orderQuantity,quantity)
+      if(orderQuantity >= minimumOrderQuantity){
+          if(orderQuantity < quantity){
+
+            // save to order to database
+            
+          }
+          else{
+              toast.error('out of stoke')
+          }
+      }
+      else{
+          toast.error('please order minimum quantity')
+      }
+  };
+
   return (
     <div>
       <h1 className="text-4xl my-16 font-bold text-primary">
         Please Fill This
       </h1>
-      <form className="w-48 lg:w-80 mx-auto">
+      <form className="w-48 lg:w-80 mx-auto" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control  mx-auto">
           <label className="label">
             <span className="label-text">Your Name</span>
@@ -25,10 +54,11 @@ const Purchase = () => {
           <label className="input-group">
             <input
               type="text"
-              name="name"
               readOnly
-              placeholder="Your Name"
+              value={user.displayName}
               className="input input-bordered w-full"
+              defaultValue="test"
+              {...register("userName")}
             />
           </label>
         </div>
@@ -38,47 +68,76 @@ const Purchase = () => {
           </label>
           <label className="input-group">
             <input
-              type="email"
-              name="email"
-              required
-              placeholder="info@site.com"
+              type="text"
+              readOnly
+              value={user.email}
               className="input input-bordered w-full"
+              defaultValue="test"
+              {...register("email")}
             />
           </label>
         </div>
         <div className="form-control  mx-auto">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Product Name</span>
           </label>
           <label className="input-group">
             <input
-              type="password"
-              name="password"
-              required
-              placeholder="Password"
+              type="text"
+              name="name"
+              readOnly
+              value={product.name}
               className="input input-bordered w-full"
+              defaultValue="test"
+              {...register("productName")}
             />
           </label>
         </div>
-        <div className="form-control mx-auto">
+        <div className="form-control  mx-auto">
           <label className="label">
-            <span className="label-text">Confirm Password</span>
+            <span className="label-text">Order Quantity</span>
           </label>
           <label className="input-group">
             <input
-              type="password"
-              name="confirmPassword"
-              required
-              placeholder="Confirm Password"
+              type="number"
               className="input input-bordered w-full"
+              defaultValue={product.minimumOrderQuantity}
+              {...register("orderQuantity")}
             />
           </label>
         </div>
+        <div className="form-control  mx-auto">
+          <label className="label">
+            <span className="label-text">Your Address</span>
+          </label>
+          <label className="input-group">
+            <input
+              type="text"
+              className="input input-bordered w-full"
+              defaultValue="Address"
+              {...register("address")}
+            />
+          </label>
+        </div>
+        <div className="form-control  mx-auto">
+          <label className="label">
+            <span className="label-text">Phone Number</span>
+          </label>
+          <label className="input-group">
+            <input
+              type="text"
+              className="input input-bordered w-full"
+              defaultValue="Phone Number"
+              {...register("phoneNumber")}
+            />
+          </label>
+        </div>
+
         <div className="form-control py-2 my-10">
           <label className="input-group">
             <input
               type="submit"
-              value="Sign Up"
+              value="Place Order"
               className="btn btn-lg w-full"
             />
           </label>
